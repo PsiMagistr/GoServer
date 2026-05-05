2f778380fbcf04ab4122501d18f6ada1c2ef6340
PsiMagistr <chago81@inbox.ru>
2026-05-05 18:13:40 +0300

    Установка всех дедлайнов в сокетах. Ограничение на размер сообщений. Установка пингов.

--- internal/game/client.go
+++ internal/game/client.go
@@ -1,9 +1,18 @@
 package game
 
 import (
+	"fmt"
+	"time"
+
 	"GoServer/internal/models"
 
 	"github.com/gorilla/websocket"
+)
+
+const (
+	writeWait  = 10 * time.Second
+	pongWait   = 60 * time.Second
+	pingPeriod = (pongWait * 9) / 10
 )
 
 type Client struct {
@@ -13,18 +22,41 @@ type Client struct {
 }
 
 func (c *Client) WritePump() {
-	defer c.Conn.Close()
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.Conn.Close()
+	}()
 	for {
-		message, ok := <-c.Send
-		if !ok {
-			c.Conn.WriteMessage(websocket.CloseMessage, []byte{})
-			return
+		select {
+		case message, ok := <-c.Send:
+			_ = c.Conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				_ = c.Conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+			err := c.Conn.WriteJSON(message)
+			if err != nil {
+				return
+			}
+		case <-ticker.C:
+			_ = c.Conn.SetWriteDeadline(time.Now().Add(writeWait))
+			err := c.Conn.WriteMessage(websocket.PingMessage, nil)
+			fmt.Println("Сердце")
+			if err != nil {
+				return
+			}
 		}
-		c.Conn.WriteJSON(message)
 	}
 }
 
 func (c *Client) ReadPump(h *Hub) {
+	c.Conn.SetReadLimit(512) // Максимальный размер сообщения от игрока (защита от спама)
+	_ = c.Conn.SetReadDeadline(time.Now().Add(pongWait))
+	c.Conn.SetPongHandler(func(string) error {
+		c.Conn.SetReadDeadline(time.Now().Add(pongWait))
+		return nil
+	})
 	defer func() {
 		h.Unregister(c.Character.ID)
 		c.Conn.Close()
