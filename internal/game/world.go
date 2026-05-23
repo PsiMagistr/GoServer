8462f9a78eef6bcdce331f07ad2d9e23550afaaa
PsiMagistr <chago81@inbox.ru>
2026-05-23 15:23:08 +0300

    Продолжаю делать перемещение между мирами. Начаток нового мира - Тотльхейм.

--- internal/game/world.go
+++ internal/game/world.go
@@ -1,11 +1,16 @@
 package game
 
+type Element struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
 type MapNode struct {
-	ID     string   `json:"id"`
-	Name   string   `json:"name"`
-	X      int      `json:"x"`
-	Y      int      `json:"y"`
-	Worlds []string `json:"worlds"`
+	ID     string    `json:"id"`
+	Name   string    `json:"name"`
+	X      int       `json:"x"`
+	Y      int       `json:"y"`
+	Worlds []Element `json:"worlds"`
 }
 
 type WorldNodes map[string]MapNode
@@ -25,55 +30,67 @@ var Universe = map[string]World{
 				ID:   "start_glade",
 				Name: "Дом родной",
 				X:    363, Y: 185,
-				Worlds: []string{},
+				Worlds: []Element{},
 			},
 			"tample": {
 				ID:   "tample",
 				Name: "Храм",
 				X:    194, Y: 180,
-				Worlds: []string{},
+				Worlds: []Element{},
 			},
 			"tavern": {
 				ID:   "tavern",
 				Name: "Таверна",
 				X:    195, Y: 269,
-				Worlds: []string{},
+				Worlds: []Element{},
 			},
 			"arena": {
 				ID:   "arena",
 				Name: "Арена",
 				X:    243, Y: 128,
-				Worlds: []string{},
+				Worlds: []Element{},
 			},
 			"shop": {
 				ID:   "shop",
 				Name: "Лавка",
 				X:    406, Y: 272,
-				Worlds: []string{},
+				Worlds: []Element{},
 			},
 			"bestiary": {
 				ID:   "bestiary",
 				Name: "Тренировочная",
 				X:    193, Y: 99,
-				Worlds: []string{},
+				Worlds: []Element{},
 			},
 			"academy": {
 				ID:   "academy",
 				Name: "Академия",
 				X:    284, Y: 241,
-				Worlds: []string{},
+				Worlds: []Element{},
 			},
 			"portal": {
 				ID:   "portal",
 				Name: "Портал",
 				X:    449, Y: 125,
-				Worlds: []string{"Благословенный край"},
+				Worlds: []Element{{ID: "totlhaim", Name: "Тотльхайм"}},
 			},
 			"armory": {
 				ID:   "armory",
 				Name: "Оружейная",
 				X:    323, Y: 101,
-				Worlds: []string{},
+				Worlds: []Element{},
+			},
+		},
+	},
+	"totlhaim": { // Второй мир Тотльхайм
+		ID:   "totlhaim",
+		Name: "Тотльхейм",
+		Points: WorldNodes{
+			"portal": {
+				ID:   "portal",
+				Name: "Портал",
+				X:    449, Y: 125,
+				Worlds: []Element{{ID: "main_city", Name: "Благословенный край"}},
 			},
 		},
 	},
